76ccb0ee553fa781e5013938e23f1be65f3abb17
RishalAggarwal <rishal.aggarwal@gmail.com>
2019-01-08 08:40:53 +0530

    Add files via upload

---
+++ Fake_news_detection/FeatureSelection.py
@@ -0,0 +1,187 @@
+# -*- coding: utf-8 -*-
+
+import DataPrep
+import spacy
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import mutual_info_classif
+from textstat.textstat import textstatistics, easy_word_set, legacy_round
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.pipeline import Pipeline
+import nltk
+import nltk.corpus 
+from nltk.tokenize import word_tokenize
+import collections
+import pickle
+
+
+
+def break_sentences(text):
+    nlp = spacy.load('en')
+    doc = nlp(text)
+    return doc.sents
+
+
+# Returns Number of Words in the text
+def word_count(text):
+    sentences = break_sentences(text)
+    words = 0
+    for sentence in sentences:
+        words += len([token for token in sentence])
+    return words
+
+
+# Returns the number of sentences in the text
+def sentence_count(text):
+    sentences = break_sentences(text)
+    return len(list(sentences))
+
+
+# Returns average sentence length
+def avg_sentence_length(text):
+    words = word_count(text)
+    sentences = sentence_count(text)
+    average_sentence_length = float(words / sentences)
+    return average_sentence_length
+
+
+
+def syllables_count(word):
+    return textstatistics().syllable_count(word)
+
+
+def avg_syllables_per_word(text):
+    syllable = syllables_count(text)
+    words = word_count(text)
+    ASPW = float(syllable) / float(words)
+    return legacy_round(ASPW, 1)
+
+
+# Return total Difficult Words in a text
+def difficult_words(text):
+    words = []
+    sentences = break_sentences(text)
+    for sentence in sentences:
+        words += [str(token) for token in sentence]
+
+    diff_words_set = set()
+
+    for word in words:
+        syllable_count = syllables_count(word)
+        if word not in easy_word_set and syllable_count >= 2:
+            diff_words_set.add(word)
+
+    return len(diff_words_set)
+
+
+
+def poly_syllable_count(text):
+    count = 0
+    words = []
+    sentences = break_sentences(text)
+    for sentence in sentences:
+        words += [token for token in sentence]
+
+    for word in words:
+        syllable_count = syllables_count(word)
+        if syllable_count >= 3:
+            count += 1
+    return count
+
+
+def flesch_reading_ease(text):
+    FRE = 206.835 - float(1.015 * avg_sentence_length(text)) - \
+          float(84.6 * avg_syllables_per_word(text))
+    return legacy_round(FRE, 2)
+
+
+def gunning_fog(text):
+    per_diff_words = (difficult_words(text) / word_count(text) * 100) + 5
+    grade = 0.4 * (avg_sentence_length(text) + per_diff_words)
+    return grade
+
+
+def smog_index(text):
+   if sentence_count(text) >= 3:
+        poly_syllab = poly_syllable_count(text)
+        SMOG = (1.043 * (30 * (poly_syllab / sentence_count(text))) ** 0.5) \
+               + 3.1291
+        return legacy_round(SMOG, 1)
+   else:
+        return 0
+
+
+def dale_chall_readability_score(text):
+
+    words = word_count(text)
+    count = word_count - difficult_words(text)
+    if words > 0:
+
+
+        per = float(count) / float(words) * 100
+
+
+    diff_words = 100 - per
+
+    raw_score = (0.1579 * diff_words) + \
+                (0.0496 * avg_sentence_length(text))
+
+
+
+    if diff_words > 5:
+        raw_score += 3.6365
+
+    return legacy_round(raw_score, 2)
+
+def get_readability(x):
+    gun_fog=[]
+    flesh=[]
+    c=1
+    for i in x:
+        gun_fog.append(gunning_fog(i))
+        flesh.append(flesch_reading_ease(i))
+        c+=1
+    return np.vstack((np.asarray(gun_fog),np.asarray(flesh)))
+
+class GrammarTransformer():
+    """
+    Convert text to counts of syntactic structure
+    """
+    def __init__(self, parser):
+        self.parser = spacy.load('en')
+
+    def transform(self, X, y=None,**transform_params):
+        return self.countgrammar(X)
+
+    def fit(self, X, y=None, **fit_params):
+        return self
+
+    def get_params(self, deep=True):
+        return {}
+
+    def countgrammar(self, texts):
+        lookup = {}
+        for i, x in enumerate(texts):
+            lookup[x] = i
+        grammar_counts = {}
+        for doc in self.parser.pipe(texts, batch_size=1000, n_threads=4):
+            counts = collections.Counter()
+            for w in doc:
+                counts[w.dep_] += 1
+            grammar_counts[doc.text] = counts
+        rv = list(range(len(texts)))
+        for text, i in lookup.items():
+            rv[i] = grammar_counts[text]
+        for i in range(len(rv)):
+            if rv[i]==i:
+                rv[i]=None
+
+        return rv
+
+
+
+
---
+++ Fake_news_detection/classifier.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+
+import DataPrep
+import numpy as np
+import pandas as pd
+import pickle
+import spacy
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import mutual_info_classif
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.pipeline import Pipeline
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import  LogisticRegression
+from sklearn.linear_model import SGDClassifier
+from sklearn import svm
+from sklearn import preprocessing
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import KFold
+from sklearn.metrics import confusion_matrix, f1_score, classification_report
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import learning_curve
+import matplotlib.pyplot as plt
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import average_precision_score
+import FeatureSelection
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+
+
+countV = CountVectorizer(token_pattern=r"!|\?|\"|\'|,|\.|;|:|-|\(|\)")
+tfidf_ngram = TfidfVectorizer(stop_words='english',ngram_range=(1,2),use_idf=True,smooth_idf=True)
+selector=SelectKBest(score_func=mutual_info_classif,k=20000)
+gram=FeatureSelection.GrammarTransformer(spacy.load('en'))
+dictvec=DictVectorizer()
+tfidftrans=TfidfTransformer()
+min_max_scaler = preprocessing.MinMaxScaler()
+
+count_punct_train=countV.fit_transform(DataPrep.train_news['Statement'].values)
+tfidf_train=tfidf_ngram.fit_transform(DataPrep.train_news['Statement'].values)
+tfidf_train=selector.fit_transform(tfidf_train,DataPrep.train_news['Label'].values)
+gram_syntax=gram.countgrammar(DataPrep.train_news['Statement'].values)
+gram_syntax= dictvec.fit_transform(gram_syntax)
+gram_syntax_train= tfidftrans.fit_transform(gram_syntax)
+gun_fog_train = pickle.load(open('gun_fog_train.pkl', 'rb'))
+gun_fog_train=np.expand_dims(gun_fog_train,0)
+flesch_train = pickle.load(open('flesch_train.pkl', 'rb'))
+flesch_train=np.expand_dims(flesch_train,0)
+readability_train=np.vstack((gun_fog_train,flesch_train))
+readability_train=np.swapaxes(readability_train,0,1)
+readability_train = min_max_scaler.fit_transform(readability_train)
+train_feature_mat=np.hstack((tfidf_train.toarray(),count_punct_train.toarray(),readability_train,gram_syntax_train.toarray()))
+
+count_punct_test=countV.transform(DataPrep.test_news['Statement'].values)
+tfidf_test=tfidf_ngram.transform(DataPrep.test_news['Statement'].values)
+tfidf_test=selector.transform(tfidf_test)
+gram_syntax_test=gram.countgrammar(DataPrep.test_news['Statement'].values)
+gram_syntax_test=dictvec.transform(gram_syntax_test)
+gram_syntax_test=tfidftrans.transform(gram_syntax_test)
+gun_fog_test = pickle.load(open('gun_fog_test.pkl', 'rb'))
+gun_fog_test=np.expand_dims(gun_fog_test,0)
+flesch_test = pickle.load(open('flesch_test.pkl', 'rb'))
+flesch_test=np.expand_dims(flesch_test,0)
+readability_test=np.vstack((gun_fog_test,flesch_test))
+readability_test=np.swapaxes(readability_test,0,1)
+readability_test = min_max_scaler.transform(readability_test)
+test_feature_mat=np.hstack((tfidf_test.toarray(),count_punct_test.toarray(),readability_test,gram_syntax_test.toarray()))
+
+
+count_punct_valid=countV.transform(DataPrep.valid_news['Statement'].values)
+tfidf_valid=tfidf_ngram.transform(DataPrep.valid_news['Statement'].values)
+tfidf_valid=selector.transform(tfidf_valid)
+gram_syntax_valid=gram.countgrammar(DataPrep.valid_news['Statement'].values)
+gram_syntax_valid=dictvec.transform(gram_syntax_valid)
+gram_syntax_valid=tfidftrans.transform(gram_syntax_valid)
+gun_fog_valid = pickle.load(open('gun_fog_valid.pkl', 'rb'))
+gun_fog_valid=np.expand_dims(gun_fog_valid,0)
+flesch_valid = pickle.load(open('flesch_valid.pkl', 'rb'))
+flesch_valid=np.expand_dims(flesch_valid,0)
+readability_valid=np.vstack((gun_fog_valid,flesch_valid))
+readability_valid=np.swapaxes(readability_valid,0,1)
+readability_valid = min_max_scaler.transform(readability_valid)
+valid_feature_mat=np.hstack((tfidf_valid.toarray(),count_punct_valid.toarray(),readability_valid[:1284],gram_syntax_valid.toarray()))
+
+
+nb=MultinomialNB()
+nb.fit(train_feature_mat,DataPrep.train_news['Label'])
+predicted_nb_test=nb.predict(test_feature_mat)
+predicted_nb_train=nb.predict(train_feature_mat)
+predicted_nb_valid=nb.predict(valid_feature_mat)
+
+LogR=LogisticRegression(penalty="l2",C=1,solver='liblinear',max_iter=3000)
+LogR.fit(train_feature_mat,DataPrep.train_news['Label'])
+predicted_LogR_test=LogR.predict(test_feature_mat)
+predicted_LogR_train=LogR.predict(train_feature_mat)
+predicted_LogR_valid=LogR.predict(valid_feature_mat)
+
+svm=svm.LinearSVC(max_iter=10000)
+svm.fit(train_feature_mat,DataPrep.train_news['Label'])
+predicted_svm_test=svm.predict(test_feature_mat)
+predicted_svm_train=svm.predict(train_feature_mat)
+predicted_svm_valid=svm.predict(valid_feature_mat)
+
+
+
+
+print('test')
+print('naive bayes')
+print(classification_report(DataPrep.test_news['Label'], predicted_nb_test))
+print('LogR')
+print(classification_report(DataPrep.test_news['Label'], predicted_LogR_test))
+print('svm')
+print(classification_report(DataPrep.test_news['Label'], predicted_svm_test))
+
+print('valid')
+print('naive bayes')
+print(classification_report(DataPrep.valid_news['Label'], predicted_nb_valid))
+print('LogR')
+print(classification_report(DataPrep.valid_news['Label'], predicted_LogR_valid))
+print('svm')
+print(classification_report(DataPrep.valid_news['Label'], predicted_svm_valid))
+
+
+min_max_scaler_file='scaler_file.pkl'
+dictvec_file='dicvec_model.pkl'
+tfidftrans_file='trans_model.pkl'
+selector_file='selector_model.pkl'
+punct_file='punct_model.pkl'
+tfidf_file='tfidf_model.pkl'
+model_file = 'final_model.pkl'
+
+pickle.dump(min_max_scaler,open(min_max_scaler_file,'wb'))
+pickle.dump(dictvec,open(dictvec_file,'wb'))
+pickle.dump(tfidftrans,open(tfidftrans_file,'wb'))
+pickle.dump(selector,open(selector_file,'wb'))
+pickle.dump(tfidf_ngram,open(tfidf_file,'wb'))
+pickle.dump(countV,open(punct_file,'wb'))
+pickle.dump((LogR),open(model_file,'wb'))
+
+
+
